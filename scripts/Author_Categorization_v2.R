c2906451a65c3e6e9ceecb7916215614e188f74a
cayelan <cayelan@vt.edu>
2018-02-15 13:45:51 -0500

    updated psuedocode

--- scripts/Author_Categorization_v2.R
+++ scripts/Author_Categorization_v2.R
@@ -1,8 +1,4 @@
-# PSUEDOCODE!!
-# - Install packages
-# - Read in Ecology_FullRecords.CSV 
-# - omit columns we don’t need, but keep the numeric identifier already in the full records CSV as each paper’s ID
-# - read in long version of keywords CSV [this needs to be checked against Nicole’s original script, now on github] that has two columns: Keywords and Affiliations
+
 # - use the for loop to assign disciplinary affiliations to each paper_ID (*not* the lumped categories of CS, MA, etc. but the level of Chemistry, Physics, etc)
 # -within the for loop, create the author affiliation data frame. Importantly, this data frame also needs to be in long format, with four columns: “Paper_ID” (this is its number), 
 #   “Affiliation_ID” (this is another numeric identifier (1, 2, 3…) for each affiliation listed in the order they are listed on the paper, even if they are repeats and they are later
@@ -11,8 +7,6 @@
 # 
 # AIK (14 Feb 18) - I suggest adding a Year column to this long format dataframe - I use that a lot
 #
-# (because there are three authors, and the listed keywords are the words in their affiliation that assigns them the Environmental Biology category from the keywords CSV)
-# 
 # -now we clean up the data frame: omit repeat affiliations within the same paper, omit government entities, omit papers that don’t have any affiliations, etc.
 # -export this data frame as an “AuthorAffiliationDatabase.CSV”
 # -now we look at each paper for our collaboration analysis- create a new for loop that goes through the data frame and assigns the lumped categories (CS, MA, etc.) [note: didn’t we get rid of ‘interdisciplinary computing’?] 
