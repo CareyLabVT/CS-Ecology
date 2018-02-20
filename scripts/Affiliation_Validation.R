f60c0307a59be13caaad1003b58f7cffa33ab99c
KJ Farrell <farrellkj2@gmail.com>
2018-02-20 12:09:16 -0500

    script to randomly sample 5% of authors for manual validation; outputs validation.csv to subfolder of output_data

---
+++ scripts/Affiliation_Validation.R
@@ -0,0 +1,16 @@
+## Pull a random subset of authors for manual checking of affiliation ##
+
+#install.packages('pacman') # Install pacman package
+pacman::p_load(tidyverse) # Load packages
+
+#### Take a random sample from author affiliations dataframe for validation ####
+val <- c('CCC','KJF','JPD','RPM','NWK','MEL','AIK') # List of validator IDs
+
+subset <- read_csv('./output_data/author_affiliations.csv') %>% # Load author affiliations
+  select(-Keyword, -AffiliationGroup) %>% # Omit algorithm-assigned affiliations
+  sample_frac(0.05, replace = FALSE) %>% # Subsample 5% of authors without replacement
+  mutate(Validator = rep(val, length = nrow(.))) %>% # Assign validator ID at random
+  arrange(Validator) %>% # Sort rows by validator ID
+  mutate(ManualAffiliation = '') # Create empty column for validator to assign affiliation
+
+write_csv(subset, './output_data/validation/validation.csv') # Export as csv to validation subfolder
