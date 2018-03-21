39955399ede822b1cf245b53fae1801aa9be6432
Kait Farrell <farrellkj2@users.noreply.github.com>
2018-03-20 21:49:14 -0400

    fix typo in validator initials

--- scripts/Affiliation_Validation.R
+++ scripts/Affiliation_Validation.R
@@ -4,7 +4,7 @@
 pacman::p_load(tidyverse) # Load packages
 
 #### Take a random sample from author affiliations dataframe for validation ####
-val <- c('CCC','KJF','JPD','RPM','NWK','MEL','AIK') # List of validator IDs
+val <- c('CCC','KJF','JPD','RPM','NKW','MEL','AIK') # List of validator IDs
 set.seed(2) # Set seed to replicate same validation draw each time
 
 subset <- read_csv('./output_data/author_affiliations.csv') %>% # Load author affiliations
