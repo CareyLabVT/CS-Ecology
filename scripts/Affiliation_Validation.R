9b34437138e27d38480fd27c14c360519d76243c
Arianna Krinos <akrinos@vt.edu>
2018-03-29 00:56:22 -0400

    Starting to put affiliation data n the proper format for checking the matched affiliation

--- scripts/Affiliation_Validation.R
+++ scripts/Affiliation_Validation.R
@@ -16,3 +16,27 @@ subset <- read_csv('./output_data/author_affiliations.csv') %>% # Load author af
   mutate(ManualKeyword = '', ManualAffiliation = '') # Add empty column for validator to assign keyword & affiliation
   
 write_csv(subset, './output_data/validation/validation.csv') # Export as csv to validation subfolder
+
+all_affildata = read_csv('./output_data/author_affiliations.csv')
+for (t in 1:length(val)) {
+  if (val[t] != 'JPD') {
+  currFile = paste0('./output_data/validation/validation_', val[t], '.csv')
+  curr_data = read_csv(currFile) %>%
+              filter(Validator == val[t])
+  if (t == 1) {
+    all_valdata = curr_data
+  } else {
+    all_valdata = all_valdata %>%
+                  bind_rows(curr_data)
+  }
+  }
+}
+
+all_valdata = all_valdata %>%
+              arrange(Paper_ID) %>%
+              mutate(ID_affil = paste0(all_valdata$Paper_ID, '_', all_valdata$Author_ID, '_', all_valdata$Affiliation_ID))
+
+all_affildata = all_affildata %>%
+                arrange(Paper_ID) %>%
+                mutate(ID_affil = paste0(all_affildata$Paper_ID, '_', all_affildata$Author_ID, '_', all_affildata$Affiliation_ID)) %>%
+                filter(ID_affil %in% all_valdata$ID_affil)
