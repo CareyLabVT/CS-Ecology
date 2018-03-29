cba2021a8ad8f2d22a9f1a98b27c4c7841c4d097
KJ Farrell <farrellkj2@gmail.com>
2018-03-29 10:21:48 -0400

    streamline tallying of ES & other collaborations per year

--- scripts/Analysis_Plotting.R
+++ scripts/Analysis_Plotting.R
@@ -2,11 +2,11 @@
 #' ("author_affiliations.csv", output from "Author_Categorization.R" script)
 #' Written by AIK and KJF, last updated 29 March 2018
 
-#### Load packages ####
+# Load packages ####
 # install.packages('pacman') 
 pacman::p_load(tidyverse) # Install and load libraries
 
-#### Analysis of Collaborations Over Time #### 
+# Analysis of Collaborations Over Time #### 
 affiliation_groups <- read_csv('./output_data/author_affiliations.csv') %>% # Load author categorizations
   filter(Year >= 1969 & Year <= 2016) %>% # Select for focal years
   group_by(Year, AffiliationGroup) %>%
@@ -22,62 +22,49 @@ CS_papers_by_year <- CS_authored_papers %>%
   summarize(Annual_CS_Papers = n_distinct(Paper_ID))
 
 # Authors per affiliation for each paper
-affiliation_combos = affiliation_groups %>%
+affiliation_combos <- affiliation_groups %>%
   spread(key = AffiliationGroup, value = Author_Count)
 
-# Count number of papers per year with collaboration between CS and other disciplines 
-CS_collaborations <- left_join((affiliation_combos %>%    # Comp Sci and Env Sci
+# Count number of papers per year with collaboration between CS and other disciplines ####
+CS_collaborations <- full_join((affiliation_combos %>%    # Comp Sci and Env Sci
                            filter(CS >= 1 & ES >= 1) %>% 
                            summarize(CSES = n())), 
                         (affiliation_combos %>%           # Comp Sci and Eng
                            filter(CS >= 1 & EG >= 1) %>% 
                            summarize(CSEG = n()))) %>%
-  left_join(., (affiliation_combos %>%                    # Comp Sci and Math
+  full_join(., (affiliation_combos %>%                    # Comp Sci and Math
                   filter(CS >= 1 & MA >=1) %>% 
                   summarize(CSMA = n()))) %>%
-  left_join(., (affiliation_combos %>%                    # Comp Sci and Physics
+  full_join(., (affiliation_combos %>%                    # Comp Sci and Physics
                   filter(CS >= 1 & PS >=1) %>% 
                   summarize(CSPS = n()))) %>%
-  left_join(., (affiliation_combos %>%                    # Comp Sci and Social Sci
+  full_join(., (affiliation_combos %>%                    # Comp Sci and Social Sci
                   filter(CS >= 1 & SS >=1) %>% 
                   summarize(CSSS = n())))
 
-# Same process for Environmental Biologists collaborating with other disciplines 
-tallied_ESCS = affiliation_combos %>%
-  group_by(Year) %>%
-  count(ES, CS) %>%
-  filter(ES == 1 & CS == 1) %>%
-  rename(ESCS = n)
-tallied_ESEG = affiliation_combos %>%
-  count(ES, EG) %>%
-  filter(ES == 1 & EG == 1) %>%
-  rename(ESEG = n)
-tallied_ESMA = affiliation_combos %>%
-  count(ES, MA) %>%
-  filter(ES == 1 & MA == 1) %>%
-  rename(ESMA = n)
-tallied_ESPS = affiliation_combos %>%
-  count(ES, PS) %>%
-  filter(ES == 1 & PS == 1) %>%
-  rename(ESPS = n)
-tallied_ESSS = affiliation_combos %>%
-  count(ES, SS) %>%
-  filter(ES == 1 & SS == 1) %>%
-  rename(ESSS = n)
+# Count number of papers per year with collaboration between Env. Sci and other disciplines ####
+ES_collaborations <- full_join((affiliation_combos %>%    # Env Sci and Comp Sci
+                                  filter(ES >= 1 & CS >= 1) %>% 
+                                  summarize(ESCS = n())), 
+                               (affiliation_combos %>%    # Env Sci and Eng
+                                  filter(ES >= 1 & EG >= 1) %>% 
+                                  summarize(ESEG = n()))) %>%
+  full_join(., (affiliation_combos %>%                    # Env Sci and Math
+                  filter(ES >= 1 & MA >=1) %>% 
+                  summarize(ESMA = n()))) %>%
+  full_join(., (affiliation_combos %>%                    # Env Sci and Physics
+                  filter(ES >= 1 & PS >=1) %>% 
+                  summarize(ESPS = n()))) %>%
+  full_join(., (affiliation_combos %>%                    # Env Sci and Social Sci
+                  filter(ES >= 1 & SS >=1) %>% 
+                  summarize(ESSS = n()))) %>%
+  mutate(Total_Collab_Papers = rowSums(.[2:6], na.rm=T))
 
-ESandothers = tallied_ESCS %>%
-  full_join(tallied_ESEG) %>%
-  full_join(tallied_ESMA) %>%
-  full_join(tallied_ESPS) %>%
-  full_join(tallied_ESSS) %>%
-  select(c(-CS, -ES, -EG, -MA, -PS, -SS)) %>%
-  mutate(Sum = sum(ESEG, ESMA, ESPS, ESSS, ESCS, na.rm = TRUE))
+gatheredES <- ES_collaborations %>%
+             gather(key = Collaboration, value = Num_Papers, ESCS:ESSS, -Total_Collab_Papers) %>%
+             mutate(RelativeFreq = Num_Papers / Total_Collab_Papers)
 
-gatheredES = ESandothers %>%
-             gather(key = ESMatch, value = count,
-                      ESCS, ESEG, ESMA, ESPS, ESSS, -Sum) %>%
-             mutate(relfreq = count / Sum)
-
+# Plot frequencies of collaborations among groups ####
 colorsnew = c("red", gray.colors(4, start = 0.3, end = 0.9, gamma = 2.2, alpha = NULL)) # grayscale for non-CS, red for CS
 #colorsnew[5] = "white" # can change individual colors 
 names = unique(gatheredES$ESMatch)
