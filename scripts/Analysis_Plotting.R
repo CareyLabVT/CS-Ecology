b0209ca71c2e52a89401415843567d744d785bac
farrellkj2 <farrellkj2@gmail.com>
2018-07-24 18:19:56 -0400

    updating analyses to calculate metrics for updated text

--- scripts/Analysis_Plotting.R
+++ scripts/Analysis_Plotting.R
@@ -1,27 +1,50 @@
-#' Use categorized author affiliations to estimate and plot collaboration rates
-#' ("author_affiliations.csv", output from "Author_Categorization.R" script)
-#' Written by AIK and KJF, last updated 29 March 2018
+# Use assigned author affiliations to estimate and plot collaboration rates
+# ("author_affiliations.csv", output from "Author_Categorization.R" script)
 
-# Load packages ####
+##### Load packages and data ####
 # install.packages('pacman') 
 pacman::p_load(tidyverse, cowplot) # Install and load libraries
 author_affiliations <- read_csv('./output_data/author_affiliations.csv')
 
-# Load author categorization and count affiliations per paper #### 
-affiliation_groups <- author_affiliations %>% 
-  filter(Year >= 1969 & Year <= 2016) %>% # Select for focal years
+raw <-  read.csv("./raw_data/Ecology_FullRecords.csv", na.strings = c("", "NA")) %>% # Load Web of Science entries
+  select(paper_ID, AUTHOR2, Affiliation1, Affiliation2, YEAR, DOI) %>%  # Retain only these columns
+  rename(AUTHOR = AUTHOR2)
+
+##### Calculate author and paper metrics ####
+original_papers <- raw %>% # Load Web of Science records
+  filter(!is.na(Affiliation1) | !is.na(Affiliation2)) %>% # Omit papers with no affiliation information
+  filter(YEAR < 2017) # Omit 2017 papers (1972 - 2016 only)
+
+# Number of papers from Web of Science that can be linked to affiliations
+print(n_distinct(original_papers$paper_ID)) # ("total articles analyzed" in results)
+
+papers_retained <- author_affiliations %>% 
+  filter(AffiliationGroup != "Remove")   # Omit gov't or industry authors
+
+# Number of papers with complete, non-"Remove" author affiliations
+print(n_distinct(papers_retained$Paper_ID)) # (Number included in affiliation analysis)
+
+# Total authors with academic affiliations
+print(n_distinct(papers_retained$Author)) 
+
+# Count affiliations per paper per year
+affiliation_groups <- papers_retained %>% 
   group_by(Year, AffiliationGroup) %>%
   count(AffiliationGroup, Paper_ID) %>% # Count authors per affiliation for each paper
-  rename(Author_Count = n)
+  rename(Author_Count = n) %>%
+  select(Paper_ID, Year, AffiliationGroup, Author_Count)
 
-# Calculate author and paper metrics ####
-# Total authors with non-governmental affiliations
-authors_retained <- author_affiliations %>% 
-  filter(Year >= 1969 & Year <= 2016, # Select for focal years
-         AffiliationGroup != "Remove") %>% # Omit government-affiliated authors
-  summarize(Authors_Retained = n_distinct(Author_ID))
-
-print(authors_retained)
+# Estimate mean number of authors per paper in 1st and last decade of analysis
+Authors_over_time <- affiliation_groups %>%
+  group_by(Paper_ID, Year) %>%
+  summarize(Authors_per_paper = sum(Author_Count)) %>%
+  mutate(Decade = ifelse(Year < 1983, "Start", 
+                     ifelse(Year > 2005, "End", NA))) %>%
+  group_by(Decade) %>%
+  summarize(Mean_Author_Count = round(mean(Authors_per_paper, na.rm=T),1), 
+            SD = round(sd(Authors_per_paper, na.rm=T),1), 
+            Papers = n_distinct(Paper_ID),
+            SE = round(SD / sqrt(Papers)),2)
 
 # Total papers per year: 1972-2016
 Papers_per_year <- affiliation_groups %>%
@@ -32,32 +55,22 @@ Papers_per_year <- affiliation_groups %>%
 CS_authored_papers <- affiliation_groups %>%
            filter(AffiliationGroup == "CS")
 
+# Number of papers with a CS-affiliated author
+print(n_distinct(CS_authored_papers$Paper_ID)) 
+
+# Number of CS-affiliated papers by year
 CS_papers_by_year <- CS_authored_papers %>%
   group_by(Year) %>%
   summarize(Annual_CS_Papers = n_distinct(Paper_ID))
 
-# Group each paper by author affiliations
-# Authors per affiliation for each paper
+# Group each paper by author affiliations ####
+# Authors per affiliation group for each paper
 affiliation_combos <- affiliation_groups %>%
   spread(key = AffiliationGroup, value = Author_Count)
 
-# Count papers per year with collaboration between Comp Sci and other disciplines ####
-CS_collaborations <- full_join((affiliation_combos %>%    # Comp Sci and Env Sci
-                           filter(CS >= 1 & ES >= 1) %>% 
-                           summarize(CSES = n())), 
-                        (affiliation_combos %>%           # Comp Sci and Eng
-                           filter(CS >= 1 & EG >= 1) %>% 
-                           summarize(CSEG = n()))) %>%
-  full_join(., (affiliation_combos %>%                    # Comp Sci and Math
-                  filter(CS >= 1 & MA >=1) %>% 
-                  summarize(CSMA = n()))) %>%
-  full_join(., (affiliation_combos %>%                    # Comp Sci and Physics
-                  filter(CS >= 1 & PS >=1) %>% 
-                  summarize(CSPS = n()))) %>%
-  full_join(., (affiliation_combos %>%                    # Comp Sci and Social Sci
-                  filter(CS >= 1 & SS >=1) %>% 
-                  summarize(CSSS = n()))) %>%
-  mutate(Total_Collab_Papers = rowSums(.[2:6], na.rm=T)) # Sum of collaborative papers per year
+# Count papers per year with collaboration between Comp Sci and ES ####
+CS_collaborations <- affiliation_combos %>% # Comp Sci and Env Sci
+  filter(CS >= 1 & ES >= 1) %>% summarize(CSES = n())
 
 # Count papers per year with collaboration between Env Sci and other disciplines ####
 ES_collaborations <- full_join((affiliation_combos %>%    # Env Sci and Comp Sci
@@ -75,12 +88,26 @@ ES_collaborations <- full_join((affiliation_combos %>%    # Env Sci and Comp Sci
   full_join(., (affiliation_combos %>%                    # Env Sci and Social Sci
                   filter(ES >= 1 & SS >=1) %>% 
                   summarize(ESSS = n()))) %>%
-  mutate(Total_Collab_Papers = rowSums(.[2:6], na.rm=T)) %>% # Sum of collaborative papers per year
-  full_join(., Papers_per_year) 
+  mutate(Total_Collab_Papers = rowSums(.[2:6], na.rm=T),
+         NonCS_Collab_Papers = Total_Collab_Papers - ESCS) %>% # Sum of collaborative papers per year
+  full_join(., Papers_per_year) %>% # Add column of total papers per year
+  mutate(ES_Only_Papers = Total_Annual_Papers - Total_Collab_Papers)
+
+# Estimate proportion of papers that are collaborative between ES and anyone non-ES 
+Interdisc_collab <- ES_collaborations %>%
+  group_by(Year) %>%
+  #mutate(Percent_collab = Total_Collab_Papers / Total_Annual_Papers) %>%
+  mutate(Decade = ifelse(Year < 1983, "Start", 
+                         ifelse(Year > 2005, "End", NA))) %>%
+  group_by(Decade) %>%
+  summarize(Mean_Collab_Papers = round(mean(Total_Collab_Papers, na.rm=T),1),
+            Mean_Total_Papers = round(mean(Total_Annual_Papers, na.rm=T),1),
+            Prop_Collab = round((Mean_Collab_Papers / Mean_Total_Papers * 100),1))
+  
 
 ES_collab_frequency <- ES_collaborations %>%
              gather(key = Collaboration, value = Num_Collab_Papers, ESCS:ESSS, 
-                    -(Total_Collab_Papers:Total_Annual_Papers)) %>%
+                    -(Total_Collab_Papers:ES_Only_Papers)) %>%
              mutate(RelativeFreq = Num_Collab_Papers / Total_Collab_Papers,
                     OverallFreq = Num_Collab_Papers / Total_Annual_Papers,
                     Collaboration = factor(Collaboration,
@@ -98,10 +125,10 @@ colors = c("gray20", "gray35", "gray60", "gray75", "red") # Red for CS collabora
 
 # Plot total papers and collaborative papers per year ####
 prop_collab <- left_join(ES_collaborations, CS_papers_by_year) %>% 
-  select(Year, Total_Collab_Papers, Total_Annual_Papers, Annual_CS_Papers) %>%
-  gather(Metric, Value, Total_Collab_Papers:Annual_CS_Papers) %>%
-  mutate(Metric = factor(Metric, levels= c("Total_Annual_Papers", "Total_Collab_Papers", "Annual_CS_Papers"), 
-                                           labels = c("Total", "Total Collaborative", 
+  select(Year, NonCS_Collab_Papers, Total_Annual_Papers, Annual_CS_Papers) %>%
+  gather(Metric, Value, NonCS_Collab_Papers:Annual_CS_Papers) %>%
+  mutate(Metric = factor(Metric, levels= c("Total_Annual_Papers", "NonCS_Collab_Papers", "Annual_CS_Papers"), 
+                                           labels = c("Total Analyzed", "Non-Computer Science Collaborative", 
                                                       "Computer Science Collaborative")))
 
 # Paper Figure 1A
@@ -126,7 +153,7 @@ ggplot(ES_collaborations, aes(x = Year, y =  round((Total_Collab_Papers / Total_
 # Frequency of collaborations among all papers: Figure 1B
 b <- ggplot(ES_collab_frequency, aes(x = Year, y = RelativeFreq, fill = Collaboration)) + mytheme + 
   geom_col() + 
-  labs(x = "Year", y = "Proportion of collaborative papers") + 
+  labs(x = "Year", y = "Proportion of interdisciplinary collaborative papers") + 
   scale_x_continuous(expand = c(0,0), limits = c(1973, 2016), breaks=seq(1975,2015,5)) +
   scale_y_continuous(expand = c(0,0)) + 
   scale_fill_manual("Discipline", values = colors) 
@@ -134,11 +161,10 @@ b <- ggplot(ES_collab_frequency, aes(x = Year, y = RelativeFreq, fill = Collabor
 # Frequency within Collaborative Papers: Env Sci and other disciplines 
 ggplot(ES_collab_frequency, aes(x = Year, y = OverallFreq, fill = Collaboration)) + mytheme + 
   geom_col() + 
-  labs(x = "Year", y = "Overall frequency") + 
+  labs(x = "Year", y = "Proportion of analyzed papers") + 
   scale_x_continuous(expand = c(0,0), limits = c(1973, 2016), breaks=seq(1975,2015,5)) +
   scale_y_continuous(expand = c(0,0), limits=c(0,0.25)) + 
   scale_fill_manual("Discipline", values = colors) 
-
 
 # Figure 1 panel: ####
 plot_grid(a, b, nrow = 1, labels = c('A', 'B'), rel_widths = c(1, 1.4))
